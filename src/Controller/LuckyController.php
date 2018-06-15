5d04ee7f2835ba6081540d853933b9b49b36c508
Rida Ridane <ext-rida.ridane@tessi.fr>
2018-06-15 15:53:04 +0200

    Translation works

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -4,10 +4,11 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class LuckyController extends Controller
 {
-    public function number()
+    public function number(Request $request)
     {
         $number = mt_rand(0, 100);
 
