083ab747246327312a9a8b38cfe377d0e0e2c757
rridane <rida.ridane@gmail.com>
2018-08-25 21:14:46 +0200

    STABLE VERSION : hwiauthfacebook works

--- src/Security/Core/User/FOSUBUserProvider.php
+++ src/Security/Core/User/FOSUBUserProvider.php
@@ -7,94 +7,69 @@ use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
 use HWI\Bundle\OAuthBundle\Security\Core\User\FOSUBUserProvider as BaseClass;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-
-// Source: https://gist.github.com/danvbe/4476697
-
-class FOSUBUserProvider extends BaseClass {
-
-    public function connect(UserInterface $user, UserResponseInterface $response) {
-
+class FOSUBUserProvider extends BaseClass
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function connect(UserInterface $user, UserResponseInterface $response)
+    {
         $property = $this->getProperty($response);
-
         $username = $response->getUsername();
-
-        // On connect, retrieve the access token and the user id
+        //on connect - get the access token and the user ID
         $service = $response->getResourceOwner()->getName();
-
-        $setter = 'set' . ucfirst($service);
-        $setter_id = $setter . 'Id';
-        $setter_token = $setter . 'AccessToken';
-
-        // Disconnect previously connected users
+        $setter = 'set'.ucfirst($service);
+        $setter_id = $setter.'Id';
+        $setter_token = $setter.'AccessToken';
+        //we "disconnect" previously connected users
         if (null !== $previousUser = $this->userManager->findUserBy(array($property => $username))) {
             $previousUser->$setter_id(null);
             $previousUser->$setter_token(null);
             $this->userManager->updateUser($previousUser);
         }
-
-        // Connect using the current user
+        //we connect current user
         $user->$setter_id($username);
         $user->$setter_token($response->getAccessToken());
         $this->userManager->updateUser($user);
     }
-
-    public function loadUserByOAuthUserResponse(UserResponseInterface $response) {
-
-        dump($response);
-        die;
-
-        $data = $response->getResponse();
+    /**
+     * {@inheritdoc}
+     */
+    public function loadUserByOAuthUserResponse(UserResponseInterface $response)
+    {
         $username = $response->getUsername();
-        $email = $response->getEmail() ? $response->getEmail() : $username;
-        $user = $this->userManager->findUserBy(array($this->getProperty($response) => $username));
+        $nom = $response->getUsername();
+        $prenom = $response->getUsername();
 
-        // If the user is new
+        $user = $this->userManager->findUserBy(array($this->getProperty($response) => $username));
+        //when the user is registrating
         if (null === $user) {
             $service = $response->getResourceOwner()->getName();
-            $setter = 'set' . ucfirst($service);
-            $setter_id = $setter . 'Id';
-            $setter_token = $setter . 'AccessToken';
+            $setter = 'set'.ucfirst($service);
+
+            $setter_id = $setter.'Id';
+            $setter_token = $setter.'AccessToken';
             // create new user here
             $user = $this->userManager->createUser();
             $user->$setter_id($username);
             $user->$setter_token($response->getAccessToken());
-
             //I have set all requested data with the user's username
             //modify here with relevant data
-            $user->setUsername($this->generateRandomUsername($username, $response->getResourceOwner()->getName()));
-            $user->setEmail($email);
+            $user->setUsername($username);
+            $user->setEmail($username);
             $user->setPassword($username);
             $user->setEnabled(true);
+            $user->setNom($nom);
+            $user->setPrenom($prenom);
             $this->userManager->updateUser($user);
             return $user;
         }
-
-        // If the user exists, use the HWIOAuth
+        //if user exists - go with the HWIOAuth way
         $user = parent::loadUserByOAuthUserResponse($response);
-
         $serviceName = $response->getResourceOwner()->getName();
-
         $setter = 'set' . ucfirst($serviceName) . 'AccessToken';
-
-        // Update the access token
+        //update access token
         $user->$setter($response->getAccessToken());
-
         return $user;
-    }
-
-    /**
-     * Generates a random username with the given
-     * e.g 12345_github, 12345_facebook
-     *
-     * @param string $username
-     * @param type $serviceName
-     * @return type
-     */
-    private function generateRandomUsername($username, $serviceName){
-        if(!$username){
-            $username = "user". uniqid((rand()), true) . $serviceName;
-        }
-
-        return $username. "_" . $serviceName;
     }
 }
