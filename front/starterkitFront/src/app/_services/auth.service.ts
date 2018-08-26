b470ca06fa7dbc52de04198804ea07117f2fe9f7
rridane <rida.ridane@gmail.com>
2018-08-26 18:56:02 +0200

    UNSTABLE FRONT - STABLE BACK - Save progress

---
+++ front/starterkitFront/src/app/_services/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  getUserDetails(){
+    // post the details to API server return info if correct
+  }
+
+}
